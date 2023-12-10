7a35df0ccfc11669ed8e1a6d2d3bff9e494b387e
omkaralmale <omkaralmale@gmail.com>
2023-12-10 13:37:30 +0530

    send mail

--- src/components/auth/ForgotPassword.js
+++ src/components/auth/ForgotPassword.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Card, Form } from "react-bootstrap";
-// import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 const ForgotPassword = () => {
-  const API_KEY = "AIzaSyBtmDXCvrD-2FXli9q45y819O4fB10sh1M";
+  const nav = useNavigate();
+  const API_KEY = "AIzaSyDjbT5nCnlMy92WAq1qWTCJXib11oX2J6c";
+  const userEmail = localStorage.getItem("userEmail");
   const handleForgot = async (event) => {
     event.preventDefault();
-    const userEmail = localStorage.getItem("userEmail");
+
     try {
       const response = await fetch(
         `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${API_KEY}`,
@@ -20,6 +22,7 @@ const ForgotPassword = () => {
           "Content-Type": "application/json",
         }
       );
+
       if (!response.ok) {
         const data = await response.json();
         throw new Error(data);
@@ -27,10 +30,20 @@ const ForgotPassword = () => {
 
       const data = await response.json();
       alert("Check your  Mail", data);
+      nav("/login");
     } catch (error) {
       console.log(error);
     }
   };
+
+  const divStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  };
+
   return (
     <>
       <div
@@ -38,25 +51,36 @@ const ForgotPassword = () => {
           display: "flex",
           justifyContent: "center",
           marginTop: "50px",
+          fontFamily: "Quicksand",
         }}
       >
-        <Card style={{ width: "30rem" }}>
+        <Card style={{ width: "25rem" }}>
           <Card.Body>
             <Card.Title
               style={{
                 fontSize: "28px",
+                fontFamily: "Quicksand",
                 display: "flex",
                 justifyContent: "center",
               }}
             >
-              Reset Password
+              <div style={divStyle}>
+                <h5>Log In with</h5>
+                <img
+                  src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png"
+                  alt="logo"
+                  width="90px"
+                />
+                <p style={{ fontSize: "16px", marginTop: "20px" }}>
+                  To continue, first verify that it's you
+                </p>
+              </div>
             </Card.Title>
             <Form onSubmit={handleForgot}>
               <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Email</Form.Label>
                 <Form.Control type="Email" placeholder="Email" />
               </Form.Group>
-              <Button variant="success" type="submit">
+              <Button variant="primary" type="submit">
                 Send Link
               </Button>
             </Form>
